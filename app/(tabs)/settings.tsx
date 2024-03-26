b6a4b2d0a215903a1787868fc1ccc1ca6cd477c0
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 19:03:16 +0600

    Add settings tab component

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'tamagui'
+
+export default function Settings() {
+  return (
+    <View flex={1} alignItems="center">
+      <Text fontSize={20}>Settings</Text>
+    </View>
+  )
+}
