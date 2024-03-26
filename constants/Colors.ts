87eecac1a6388615f6c3659f21a038082ff98f1a
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 19:03:40 +0600

    Add Colors.ts file

---
+++ constants/Colors.ts
@@ -0,0 +1,20 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    color: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+    selected: 'rgba(0, 0, 0, 0.1)',
+  },
+  dark: {
+    color: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
