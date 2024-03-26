75c3fb0de535cda5865aaba9ee0b7b631180dc28
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 19:03:07 +0600

    Add mock device component

---
+++ app/(tabs)/mock.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'tamagui'
+
+export default function MockDevice() {
+  return (
+    <View flex={1} alignItems="center">
+      <Text fontSize={20}>Sonicrypt mock</Text>
+    </View>
+  )
+}
