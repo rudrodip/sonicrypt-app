4717dfe4b4c24a66750cea42948ec881110b530e
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-01 23:24:54 +0600

    Fix bug in login functionality

--- components/config-form.tsx
+++ components/config-form.tsx
@@ -1,138 +1,177 @@
 import {
-  Text,
   View,
   Input,
-  YStack,
-  XStack,
-  Select,
-  Separator,
+  Form,
+  FormTrigger,
+  Button,
   Label,
+  Text,
+  Select,
   Adapt,
   Sheet,
+  YStack,
+  XStack,
 } from "tamagui";
 import { Check, ChevronDown, ChevronUp } from "@tamagui/lucide-icons";
-import { LinearGradient } from "tamagui/linear-gradient";
-import React from "react";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import ConfigContext from "../context/config-context";
+import type { Config } from "../context/config-context";
 
 export default function ConfigForm() {
-  const [selectedValue, setSelectedValue] = React.useState("mainnet");
+  const configObj = useContext(ConfigContext);
 
   return (
-    <View flex={1} alignItems="center" padding="$4">
-      <Text fontSize={20} fontWeight="bold" marginBottom="$4">
-        Sonicrypt
-      </Text>
+    <View style={styles.formContainer}>
+      <Text style={{ fontSize: 25, fontWeight: "bold" }}>Configuration</Text>
+      <Form onSubmit={() => console.log(configObj?.config)}>
+        <InputWithLabel
+          configKey="wifiSSID"
+          label="Wifi SSID"
+          placeholder="name of your wifi"
+        />
+        <InputWithLabel
+          configKey="wifiPassword"
+          label="Wifi Password"
+          placeholder="password of your wifi network"
+        />
+        <InputWithLabel
+          configKey="walletAddress"
+          label="Wallet address"
+          placeholder="your wallet address"
+        />
+        <Select
+          value="mainnet"
+          onValueChange={(val: "mainnet" | "devnet" | "testnet") =>
+            configObj?.setConfig({ ...configObj.config, network: val })
+          }
+        >
+          <Label>Network</Label>
+          <Select.Trigger width="unset" iconAfter={ChevronDown}>
+            <Select.Value placeholder="network" />
+          </Select.Trigger>
 
-      <YStack width="100%" maxWidth={500} gap="$2">
-        <Input placeholder="Wifi SSID" />
-        <Input placeholder="Wifi Password" />
-        <Input placeholder="Wallet Address" />
+          <Adapt when="sm" platform="touch">
+            <Sheet
+              native={false}
+              modal
+              dismissOnSnapToBottom
+              animationConfig={{
+                type: "spring",
+                damping: 20,
+                mass: 1.2,
+                stiffness: 250,
+              }}
+            >
+              <Sheet.Frame>
+                <Sheet.ScrollView>
+                  <Adapt.Contents />
+                </Sheet.ScrollView>
+              </Sheet.Frame>
+              <Sheet.Overlay
+                animation="lazy"
+                enterStyle={{ opacity: 0 }}
+                exitStyle={{ opacity: 0 }}
+              />
+            </Sheet>
+          </Adapt>
+          <Select.Content zIndex={200000}>
+            <Select.ScrollUpButton
+              alignItems="center"
+              justifyContent="center"
+              position="relative"
+              width="100%"
+              height="$3"
+            >
+              <YStack zIndex={10}>
+                <ChevronUp size={20} />
+              </YStack>
+            </Select.ScrollUpButton>
 
-        <YStack style={{ position: "relative" }}>
-          <Select
-            id="network"
-            value={selectedValue}
-            onValueChange={setSelectedValue}
-          >
-            <Select.Trigger f={1} iconAfter={ChevronDown}>
-              <Select.Value placeholder="Network" />
-            </Select.Trigger>
+            <Select.Viewport
+              // to do animations:
+              animation="quick"
+              animateOnly={['transform', 'opacity']}
+              enterStyle={{ o: 0, y: -10 }}
+              exitStyle={{ o: 0, y: 10 }}
+              minWidth={200}
+            >
+              <Select.Group>
+                <Select.Label>Blockchain network</Select.Label>
+                <Select.Item index={1} value="mainnet">
+                  <Select.ItemText>Main net</Select.ItemText>
+                  <Select.ItemIndicator marginLeft="auto">
+                    <Check size={16} />
+                  </Select.ItemIndicator>
+                </Select.Item>
+                <Select.Item index={1} value="devnet">
+                  <Select.ItemText>Dev net</Select.ItemText>
+                  <Select.ItemIndicator marginLeft="auto">
+                    <Check size={16} />
+                  </Select.ItemIndicator>
+                </Select.Item>
+                <Select.Item index={1} value="testnet">
+                  <Select.ItemText>Testnet net</Select.ItemText>
+                  <Select.ItemIndicator marginLeft="auto">
+                    <Check size={16} />
+                  </Select.ItemIndicator>
+                </Select.Item>
+              </Select.Group>
+            </Select.Viewport>
 
-            <Adapt when="sm" platform="touch">
-              <Sheet
-                native={true}
-                modal
-                dismissOnSnapToBottom
-                animationConfig={{
-                  type: "spring",
-                  damping: 20,
-                  mass: 1.2,
-                  stiffness: 250,
-                }}
-              >
-                <Sheet.Frame>
-                  <Sheet.ScrollView>
-                    <Adapt.Contents />
-                  </Sheet.ScrollView>
-                </Sheet.Frame>
-                <Sheet.Overlay
-                  animation="lazy"
-                  enterStyle={{ opacity: 0 }}
-                  exitStyle={{ opacity: 0 }}
-                />
-              </Sheet>
-            </Adapt>
-
-            <Select.Content zIndex={200000}>
-              <Select.ScrollUpButton
-                alignItems="center"
-                justifyContent="center"
-                position="relative"
-                width="100%"
-                height="$3"
-              >
-                <YStack zIndex={10}>
-                  <ChevronUp size={20} />
-                </YStack>
-                <LinearGradient
-                  start={[0, 0]}
-                  end={[0, 1]}
-                  fullscreen
-                  colors={["$background", "transparent"]}
-                  borderRadius="$4"
-                />
-              </Select.ScrollUpButton>
-              <Select.Viewport
-                animation="quick"
-                animateOnly={["transform", "opacity"]}
-                enterStyle={{ o: 0, y: -10 }}
-                exitStyle={{ o: 0, y: 10 }}
-                minWidth={200}
-              >
-                <Select.Group>
-                  <Select.Label>Fruits</Select.Label>
-                  <Select.Item index={1} key="mainnet" value="mainnet">
-                    <Select.ItemText>mainnet</Select.ItemText>
-                    <Select.ItemIndicator marginLeft="auto">
-                      <Check size={16} />
-                    </Select.ItemIndicator>
-                  </Select.Item>
-                  <Select.Item index={2} key="devnet" value="devnet">
-                    <Select.ItemText>devnet</Select.ItemText>
-                    <Select.ItemIndicator marginLeft="auto">
-                      <Check size={16} />
-                    </Select.ItemIndicator>
-                  </Select.Item>
-                  <Select.Item index={3} key="testnet" value="testnet">
-                    <Select.ItemText>testnet</Select.ItemText>
-                    <Select.ItemIndicator marginLeft="auto">
-                      <Check size={16} />
-                    </Select.ItemIndicator>
-                  </Select.Item>
-                </Select.Group>
-              </Select.Viewport>
-              <Select.ScrollDownButton
-                alignItems="center"
-                justifyContent="center"
-                position="relative"
-                width="100%"
-                height="$3"
-              >
-                <YStack zIndex={10}>
-                  <ChevronDown size={20} />
-                </YStack>
-                <LinearGradient
-                  start={[0, 0]}
-                  end={[0, 1]}
-                  fullscreen
-                  colors={["transparent", "$background"]}
-                  borderRadius="$4"
-                />
-              </Select.ScrollDownButton>
-            </Select.Content>
-          </Select>
-        </YStack>
-      </YStack>
+            <Select.ScrollDownButton
+              alignItems="center"
+              justifyContent="center"
+              position="relative"
+              width="100%"
+              height="$3"
+            >
+              <YStack zIndex={10}>
+                <ChevronDown size={20} />
+              </YStack>
+            </Select.ScrollDownButton>
+          </Select.Content>
+        </Select>
+        <FormTrigger asChild>
+          <Button style={{ marginTop: 15 }} backgroundColor="$accentColor" color="$background">Submit</Button>
+        </FormTrigger>
+      </Form>
     </View>
   );
 }
+
+type InputProps = {
+  configKey: keyof Config;
+  label: string;
+  placeholder: string;
+};
+
+const InputWithLabel = ({ configKey, label, placeholder }: InputProps) => {
+  const configObj = useContext(ConfigContext);
+
+  return (
+    <View style={styles.input}>
+      <Label style={{ fontSize: 15 }}>{label}</Label>
+      <Input
+        placeholder={placeholder}
+        onChangeText={(val) =>
+          configObj?.setConfig({ ...configObj.config, [configKey]: val })
+        }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formContainer: {
+    width: "100%",
+    padding: 5,
+    borderRadius: 10,
+    display: "flex",
+    flexDirection: "column",
+  },
+  input: {
+    borderRadius: 5,
+    width: "100%",
+  },
+});
