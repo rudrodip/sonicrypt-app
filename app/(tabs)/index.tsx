5c59fc0aff564be61cacc329aa3e08b584c9521d
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-01 23:24:51 +0600

    Fix bug in login functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,174 +1,147 @@
-import ConfigForm from "../../components/config-form";
-import React, { useState, useEffect } from "react";
+import { Text, View, ScrollView, Image } from "tamagui";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
-import { BleManager, Device } from "react-native-ble-plx";
-import { PermissionsAndroid, Platform } from "react-native";
-
-const bleManager = new BleManager();
+  Bluetooth,
+  BluetoothConnected,
+  BluetoothOff,
+  Wallet2,
+} from "@tamagui/lucide-icons";
+import { Link } from "expo-router";
+import QRCode from "react-native-qrcode-svg";
+import { useContext, useState } from "react";
+import ConfigContext from "../../context/config-context";
+import ConfigForm from "../../components/config-form";
 
 export default function Home() {
-  const [isScanning, setIsScanning] = useState(false);
-  const [devices, setDevices] = useState<Device[]>([]);
-  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
-
-  useEffect(() => {
-    const subscription = bleManager.onStateChange((state) => {
-      if (state === "PoweredOn") {
-        requestPermissions();
-      }
-    }, true);
-
-    return () => subscription.remove();
-  }, []);
-
-  useEffect(() => {
-    if (isScanning) {
-      bleManager.startDeviceScan(null, null, (error, device) => {
-        if (error) {
-          console.error(error);
-          setIsScanning(false);
-        } else if (device) {
-          console.log("Found device:", device.name || device.id);
-          console.log("here it is")
-          console.log(devices.map((d) => d.id));
-          setDevices((prevDevices) => {
-            const isDeviceAlreadyAdded = prevDevices.some(
-              (d) => d.id === device.id
-            );
-            if (!isDeviceAlreadyAdded) {
-              const updatedDevices = [...prevDevices, device];
-              console.log("Updated devices:", updatedDevices);
-              return updatedDevices;
-            }
-            return prevDevices;
-          });
-        }
-      });
-
-      return () => {
-        bleManager.stopDeviceScan();
-      };
-    }
-
-    return undefined;
-  }, [isScanning]);
-
-  const requestPermissions = async () => {
-    if (Platform.OS === "ios") {
-      scanDevices();
-      return true;
-    }
-    if (
-      Platform.OS === "android" &&
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-    ) {
-      const apiLevel = parseInt(Platform.Version.toString(), 10);
-
-      if (apiLevel < 31) {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-        );
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      }
-      if (
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
-      ) {
-        const result = await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        ]);
-
-        scanDevices();
-
-        return (
-          result["android.permission.BLUETOOTH_CONNECT"] ===
-            PermissionsAndroid.RESULTS.GRANTED &&
-          result["android.permission.BLUETOOTH_SCAN"] ===
-            PermissionsAndroid.RESULTS.GRANTED &&
-          result["android.permission.ACCESS_FINE_LOCATION"] ===
-            PermissionsAndroid.RESULTS.GRANTED
-        );
-      }
-    }
-    return false;
-  };
-
-  const scanDevices = () => {
-    setIsScanning(true);
-  };
-
-  const connectToDevice = async (device: Device) => {
-    try {
-      await device.connect();
-      setConnectedDevice(device);
-      await bleManager.connectToDevice(device.id);
-      const services = await device.discoverAllServicesAndCharacteristics();
-      console.log("Services:", services);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const disconnectDevice = async () => {
-    try {
-      if (connectedDevice) {
-        await connectedDevice.cancelConnection();
-        setConnectedDevice(null);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const renderDeviceItem = ({ item }: { item: Device }) => {
-    return (
-      <TouchableOpacity
-        onPress={() => connectToDevice(item)}
-        disabled={!!connectedDevice}
-      >
-        <Text>{item.name || item.id}</Text>
-      </TouchableOpacity>
-    );
-  };
+  const [bluetoothStatus, setBluetoothStatus] = useState<
+    "off" | "connected" | "disconnected"
+  >("off");
+  const [walletStatus, setWalletStatus] = useState<
+    "connected" | "disconnected"
+  >("disconnected");
+  const [walletAddress, setWalletAddress] = useState<string>("");
+  const configObj = useContext(ConfigContext);
 
   return (
-    <View>
-      <View>
-        <TouchableOpacity
-          onPress={scanDevices}
-          disabled={isScanning}
-        >
-          <Text>{isScanning ? "Scanning..." : "Scan Devices"}</Text>
-        </TouchableOpacity>
-        {devices.length > 0 ? (
-        <FlatList
-          data={devices}
-          renderItem={renderDeviceItem}
-          keyExtractor={(item, index) => `${item.id}-${index}`}
-        />
-      ) : (
-        <Text>No devices found</Text>
-      )}
-        {connectedDevice && (
-          <View>
-            <Text>
-              Connected to: {connectedDevice.name || connectedDevice.id}
-            </Text>
-            <TouchableOpacity onPress={disconnectDevice}>
-              <Text>Disconnect</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.cardContainer}>
+        <View style={styles.qrContainer}>
+          {walletAddress !== "" ? (
+            <QRCode value={walletAddress} size={200} />
+          ) : (
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          )}
+        </View>
+        <View style={styles.buttonContainer}>
+          <View
+            style={{
+              display: "flex",
+              width: "90%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: 3,
+            }}
+          >
+            <Link href="/bluetooth-modal" asChild>
+              <TouchableOpacity
+                style={{
+                  ...styles.button,
+                  backgroundColor:
+                    bluetoothStatus === "connected"
+                      ? "green"
+                      : bluetoothStatus === "off"
+                      ? "red"
+                      : "gray",
+                }}
+              >
+                {bluetoothStatus === "connected" ? (
+                  <BluetoothConnected />
+                ) : bluetoothStatus === "off" ? (
+                  <BluetoothOff />
+                ) : (
+                  <Bluetooth />
+                )}
+              </TouchableOpacity>
+            </Link>
+            <TouchableOpacity
+              style={{
+                ...styles.button,
+                backgroundColor:
+                  walletStatus === "connected" ? "green" : "gray",
+              }}
+              onPress={() => setWalletAddress("idk man")}
+            >
+              <Wallet2 />
             </TouchableOpacity>
           </View>
-        )}
+          <TouchableOpacity style={styles.qrCodeGenerateButton}>
+            <Text>Refresh</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.qrCodeGenerateButton}
+            disabled={walletStatus === "disconnected"}
+          >
+            <Text>Generate QR code</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <ConfigForm />
-    </View>
+      <ScrollView>
+        <ConfigForm />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: "100%",
+    aspectRatio: 1.5,
+    padding: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 2,
+  },
+  qrContainer: {
+    height: "85%",
+    aspectRatio: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    width: "45%",
+    height: "85%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 2,
+  },
+  button: {
+    width: "48%",
+    aspectRatio: 1,
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  qrCodeGenerateButton: {
+    width: "90%",
+    height: "30%",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
