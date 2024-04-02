a6e34d30fade83f7d04b7f744c09607c8d73f455
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 13:43:08 +0600

    Add Solana web3.js connection and config context to transactions component

--- app/(tabs)/transactions.tsx
+++ app/(tabs)/transactions.tsx
@@ -1,6 +1,12 @@
 import { Text, View } from 'tamagui'
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { useConfig } from '../../context/config-context';
 
 export default function Transactions() {
+  const { config, setConfig } = useConfig();
+
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
   return (
     <View flex={1} alignItems="center">
       <Text fontSize={20}>Transactions</Text>
