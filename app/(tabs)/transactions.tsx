25eefbf8924e40ee83ddd78272f78d1c8fd0f7d0
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 18:01:29 +0600

    Refactor Transactions component to include network selection and limit options

--- app/(tabs)/transactions.tsx
+++ app/(tabs)/transactions.tsx
@@ -1,32 +1,24 @@
-import {
-  Text,
-  View,
-  ScrollView,
-  ListItem,
-  Separator,
-  XStack,
-  YGroup,
-  Spinner,
-} from "tamagui";
+import { Button, Separator, Text, View } from "tamagui";
+import { ExternalLink } from "@tamagui/lucide-icons";
 import { FlatList } from "react-native";
 import {
   Connection,
   PublicKey,
   clusterApiUrl,
   ConfirmedSignatureInfo,
-  ParsedTransactionWithMeta
 } from "@solana/web3.js";
 import { useConfig } from "../../context/config-context";
 import { useEffect, useState } from "react";
+import SelectItems from "../../components/select-items";
 
 export default function Transactions() {
   const { config } = useConfig();
-  const [txs, setTxs] = useState<(ParsedTransactionWithMeta | null)[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [txs, setTxs] = useState<ConfirmedSignatureInfo[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [limit, setLimit] = useState(20);
   const [network, setNetwork] = useState<"mainnet-beta" | "devnet" | "testnet">(
-    "devnet"
+    "mainnet-beta"
   );
 
   const connection = new Connection(clusterApiUrl(network), "confirmed");
@@ -37,57 +29,119 @@ export default function Transactions() {
 
   const fetchTransactions = async () => {
     try {
-      setLoading(true);
+      setRefreshing(true);
       const transactions = await connection.getConfirmedSignaturesForAddress2(
         publicKey,
         {
           limit,
         }
       );
-      const parsedTransactions = await Promise.all(transactions.map(async (tx) => {
-        return await connection.getParsedTransaction(tx.signature, { commitment: "confirmed" });
-      }));
-      setTxs(parsedTransactions);
+      setTxs(transactions);
     } catch (error) {
       setError(error as string);
       console.log(error);
     } finally {
-      setLoading(false);
+      setRefreshing(false);
     }
   };
 
   useEffect(() => {
     fetchTransactions();
-  }, []);
+  }, [network, limit]);
 
-  const renderTxs = ({ item }: { item: ParsedTransactionWithMeta }) => {
+  const renderTxs = ({ item }: { item: ConfirmedSignatureInfo }) => {
     return (
-      <View>
-        <Text>helo</Text>
-      </View>
+      <>
+        <Button
+          key={item.signature}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding="$3"
+          backgroundColor="$background0"
+          borderRadius={0}
+        >
+          <Text fontSize="$3" fontFamily="$mono">
+            {item.signature.split("").slice(0, 30).join("")}...
+          </Text>
+          <ExternalLink size={20} />
+        </Button>
+        <Separator />
+      </>
     );
   };
 
   return (
     <View flex={1}>
-      {loading && (
-        <View
-          flex={1}
-          display="flex"
-          justifyContent="center"
-          alignContent="center"
-          alignItems="center"
-        >
-          <Spinner size="large" />
+      <View
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <View width="48%">
+          <SelectItems
+            label="Network"
+            items={networkItems}
+            onValueChange={(value) =>
+              setNetwork(value as "mainnet-beta" | "devnet" | "testnet")
+            }
+          />
         </View>
+        <View width="48%">
+          <SelectItems
+            label="Limit"
+            items={limitItems}
+            onValueChange={(value) => setLimit(parseInt(value))}
+          />
+        </View>
+      </View>
+      {txs.length === 0 && !refreshing && !error && (
+        <Text margin="$5" fontSize="$6" textAlign="center">
+          No transaction on {network} found ꃋᴖꃋ
+        </Text>
       )}
       <FlatList
-        data={txs.filter((tx) => tx !== null) as ParsedTransactionWithMeta[]}
+        data={txs}
         renderItem={renderTxs}
-        keyExtractor={(item) => item.transaction.message.recentBlockhash}
-        refreshing={loading}
+        keyExtractor={(item) => item.signature}
+        refreshing={refreshing}
         onRefresh={fetchTransactions}
+        style={{
+          marginTop: 10,
+        }}
       />
     </View>
   );
 }
+
+const networkItems = [
+  {
+    label: "Main Network",
+    value: "mainnet-beta",
+  },
+  {
+    label: "Dev Network",
+    value: "devnet",
+  },
+  {
+    label: "Test Network",
+    value: "testnet",
+  },
+];
+
+const limitItems = [
+  {
+    label: "20",
+    value: 20,
+  },
+  {
+    label: "50",
+    value: 50,
+  },
+  {
+    label: "100",
+    value: 100,
+  },
+];
