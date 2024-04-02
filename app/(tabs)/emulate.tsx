ae3c40a3ebd77187d28330178e3fe9c1f5bd28ce
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 18:01:22 +0600

    Add EmulateDevice component

---
+++ app/(tabs)/emulate.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'tamagui'
+
+export default function EmulateDevice() {
+  return (
+    <View flex={1} alignItems="center">
+      <View 
+        id="device"
+          
+      >
+
+      </View>
+    </View>
+  )
+}
