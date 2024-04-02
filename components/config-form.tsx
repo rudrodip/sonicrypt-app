f8986763b58d1f18c21eabaad5839f4b89847a13
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 16:11:14 +0600

    refactored config form

--- components/config-form.tsx
+++ components/config-form.tsx
@@ -6,23 +6,18 @@ import {
   Button,
   Label,
   Text,
-  Select,
-  Adapt,
-  Sheet,
-  YStack,
 } from "tamagui";
-import { Check, ChevronDown, ChevronUp } from "@tamagui/lucide-icons";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { useConfig } from "../context/config-context";
 import type { Config } from "../context/config-context";
 import { writeToDevice } from "../app/bluetooth-modal";
+import SelectItems from "./select-items";
 
 export default function ConfigForm() {
   const { config, setConfig } = useConfig();
 
   const sendMessageToDevice = async () => {
-    console.log(config);
     const device = config.device;
     const serviceUUID = config.bleServiceUUID;
     const characteristicUUID = config.bleCharacteristicUUID;
@@ -45,6 +40,25 @@ export default function ConfigForm() {
     }
   };
 
+  const handleSelectionChange = (val: string) => {
+    setConfig({...config, network: val as "mainnet-beta" | "devnet" | "testnet" })
+  }
+
+  const networks = [
+    {
+      label: "Main Network",
+      value: "mainnet-beta",
+    },
+    {
+      label: "Dev Network",
+      value: "devnet",
+    },
+    {
+      label: "Test Network",
+      value: "testnet"
+    }
+  ]
+
   return (
     <View style={styles.formContainer}>
       <Text style={{ fontSize: 25, fontWeight: "bold" }}>Configuration</Text>
@@ -64,98 +78,11 @@ export default function ConfigForm() {
           label="Wallet address"
           placeholder="your wallet address"
         />
-        <Select
-          value="mainnet"
-          onValueChange={(val: "mainnet" | "devnet" | "testnet") =>
-            setConfig({ ...config, network: val })
-          }
-        >
-          <Label>Network</Label>
-          <Select.Trigger width="unset" iconAfter={ChevronDown}>
-            <Select.Value placeholder="network" />
-          </Select.Trigger>
-
-          <Adapt when="sm" platform="touch">
-            <Sheet
-              native={false}
-              modal
-              dismissOnSnapToBottom
-              animationConfig={{
-                type: "spring",
-                damping: 20,
-                mass: 1.2,
-                stiffness: 250,
-              }}
-            >
-              <Sheet.Frame>
-                <Sheet.ScrollView>
-                  <Adapt.Contents />
-                </Sheet.ScrollView>
-              </Sheet.Frame>
-              <Sheet.Overlay
-                animation="lazy"
-                enterStyle={{ opacity: 0 }}
-                exitStyle={{ opacity: 0 }}
-              />
-            </Sheet>
-          </Adapt>
-          <Select.Content zIndex={200000}>
-            <Select.ScrollUpButton
-              alignItems="center"
-              justifyContent="center"
-              position="relative"
-              width="100%"
-              height="$3"
-            >
-              <YStack zIndex={10}>
-                <ChevronUp size={20} />
-              </YStack>
-            </Select.ScrollUpButton>
-
-            <Select.Viewport
-              // to do animations:
-              animation="quick"
-              animateOnly={["transform", "opacity"]}
-              enterStyle={{ o: 0, y: -10 }}
-              exitStyle={{ o: 0, y: 10 }}
-              minWidth={200}
-            >
-              <Select.Group>
-                <Select.Label>Blockchain network</Select.Label>
-                <Select.Item index={1} value="mainnet">
-                  <Select.ItemText>Main net</Select.ItemText>
-                  <Select.ItemIndicator marginLeft="auto">
-                    <Check size={16} />
-                  </Select.ItemIndicator>
-                </Select.Item>
-                <Select.Item index={1} value="devnet">
-                  <Select.ItemText>Dev net</Select.ItemText>
-                  <Select.ItemIndicator marginLeft="auto">
-                    <Check size={16} />
-                  </Select.ItemIndicator>
-                </Select.Item>
-                <Select.Item index={1} value="testnet">
-                  <Select.ItemText>Testnet net</Select.ItemText>
-                  <Select.ItemIndicator marginLeft="auto">
-                    <Check size={16} />
-                  </Select.ItemIndicator>
-                </Select.Item>
-              </Select.Group>
-            </Select.Viewport>
-
-            <Select.ScrollDownButton
-              alignItems="center"
-              justifyContent="center"
-              position="relative"
-              width="100%"
-              height="$3"
-            >
-              <YStack zIndex={10}>
-                <ChevronDown size={20} />
-              </YStack>
-            </Select.ScrollDownButton>
-          </Select.Content>
-        </Select>
+        <SelectItems 
+          label="Network"
+          items={networks}
+          onValueChange={handleSelectionChange}
+        />
         <FormTrigger asChild>
           <Button
             style={{ marginTop: 15 }}
