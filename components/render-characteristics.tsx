5e6f3bf3d542f626ff88166d6d3cf93ed91a12ef
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 16:11:33 +0600

    added toast support

--- components/render-characteristics.tsx
+++ components/render-characteristics.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { View } from "tamagui";
-import { Alert, FlatList, TouchableOpacity } from "react-native";
+import { View, YStack } from "tamagui";
+import { Alert, TouchableOpacity } from "react-native";
 import { Text } from "tamagui";
 import { Service, Characteristic } from "react-native-ble-plx";
 import { useConfig } from "../context/config-context";
 import { useNavigation } from "expo-router";
+import { Toast, useToastController, useToastState } from '@tamagui/toast'
 
 const RenderCharacteristicsComponent = ({ service }: { service: Service }) => {
+  const toast = useToastController()
   const [characteristics, setCharacteristics] = useState<Characteristic[]>([]);
   const { config, setConfig } = useConfig();
   const [loading, setLoading] = useState(true);
@@ -37,10 +39,11 @@ const RenderCharacteristicsComponent = ({ service }: { service: Service }) => {
             bleServiceUUID: service.uuid,
             bleCharacteristicUUID: item.uuid,
           });
-          Alert.alert("Characteristic selected");
+          toast.show("Characteristic selected", { duration: 1000, native: true })
           goBack();
         }}
       >
+        <CurrentToast />
         <Text marginLeft="$3" marginVertical="$1.5" fontSize="$2">
           {item.uuid}
         </Text>
@@ -63,3 +66,29 @@ const RenderCharacteristicsComponent = ({ service }: { service: Service }) => {
   );
 };
 export default RenderCharacteristicsComponent;
+
+const CurrentToast = () => {
+  const currentToast = useToastState()
+
+  if (!currentToast || currentToast.isHandledNatively) return null
+  return (
+    <Toast
+      key={currentToast.id}
+      duration={currentToast.duration}
+      enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
+      exitStyle={{ opacity: 0, scale: 1, y: -20 }}
+      y={0}
+      opacity={1}
+      scale={1}
+      animation="100ms"
+      viewportName={currentToast.viewportName}
+    >
+      <YStack>
+        <Toast.Title>{currentToast.title}</Toast.Title>
+        {!!currentToast.message && (
+          <Toast.Description>{currentToast.message}</Toast.Description>
+        )}
+      </YStack>
+    </Toast>
+  )
+}
