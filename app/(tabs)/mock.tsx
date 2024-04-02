cd4583fa4d36b41865bb5594993b502c58535d81
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 18:01:25 +0600

    Remove unused mock device component

--- app/(tabs)/mock.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'tamagui'
-
-export default function MockDevice() {
-  return (
-    <View flex={1} alignItems="center">
-      <Text fontSize={20}>Sonicrypt mock</Text>
-    </View>
-  )
-}
