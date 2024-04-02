e66b7e2f7287139d3b546101f226e5f251d7cd3f
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 18:39:26 +0600

    Add network parameter and transaction details fetching logic

--- app/[signature].tsx
+++ app/[signature].tsx
@@ -1,13 +1,42 @@
 
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'tamagui';
+import { Text, View, Spinner } from 'tamagui';
+import { useEffect, useState } from 'react';
+import {
+  Connection,
+  clusterApiUrl,
+  ParsedTransactionWithMeta
+} from "@solana/web3.js";
 
 export default function Page() {
-  const { signature } = useLocalSearchParams();
+  const { signature, network } = useLocalSearchParams();
+  console.log(signature, network);
+  const [loading, setLoading] = useState(false);
+  const [txDetails, setTxDetails] = useState<ParsedTransactionWithMeta | null>(null);
+
+  const connection = new Connection(clusterApiUrl(network as "mainnet-beta" | "testnet" | "devnet"), "confirmed");
+
+  const fetchTransactionDetails = async () => {
+    try {
+      setLoading(true);
+      const details = await connection.getParsedTransaction(signature as string, "confirmed");
+      setTxDetails(details);
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    if (!signature) return;
+    fetchTransactionDetails();
+  }, [signature, network]);
 
   return (
     <View flex={1} alignItems='center'>
       <Text>Signature: {signature}</Text>
+      <Text>Network: {network}</Text>
+      {loading && <Spinner size="large" color="$color" />}
     </View>
   );
 }
